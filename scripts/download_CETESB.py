7ed0f92b10ee59b90768ecfbc40feadb634e7fb4
Alejandro D <aperalta@usp.br>
2026-04-12 12:24:56 -0300

    Adding scripts post and namelists

---
+++ scripts/download_CETESB.py
@@ -0,0 +1,56 @@
+# Downloading meteorological and air quality parameters from CETESB stations
+# conda activate lapat
+import numpy as np
+import pandas as pd
+import qualar as qr
+import sys, os
+import requests
+import pickle as pkl
+
+#%%
+cetesb_stations = pd.read_csv('../namelists/stations.csv')
+
+print(cetesb_stations)
+start = int(input('start station (number): '))
+cetesb_stations = cetesb_stations.iloc[start:,:].head(15)
+print(cetesb_stations)
+
+obs_dates = pd.DataFrame({'date': pd.date_range(input('date (YYYY-MM-DD):'),
+                          periods=int(input('days:'))*24, freq='H')})
+obs_dates['date_qualar']=obs_dates['date'].dt.strftime('%d/%m/%Y')
+
+cetesb_login = 'adelgado@iag.usp.br' # input('login: ') 
+
+if len(sys.argv) < 2 and not 'RDAPSWD' in os.environ:
+    try:
+        import getpass
+        input = getpass.getpass
+    except:
+        try:
+            input = raw_input
+        except:
+            pass
+
+    cetesb_password = input('Password: ')
+else:
+    try:
+       cetesb_password = sys.argv[1]
+    except:
+        cetesb_password = os.environ['RDAPSWD']
+
+start_date = obs_dates.date_qualar.values[0]
+end_date = obs_dates.date_qualar.values[-1]
+
+print(obs_dates)
+
+for c,n in zip(cetesb_stations.code,cetesb_stations.name):
+    print('Downloading poll ' + n + ' Station')
+    qr.all_photo(cetesb_login, cetesb_password, start_date, end_date,
+                 c, csv_photo=False, path='./')
+    print('Downloading meteo ' + n + ' Station')
+    qr.all_met(cetesb_login, cetesb_password,  start_date, end_date,
+               c, csv_met=False, path='./')
+
+print("""
+Successfully
+""")
